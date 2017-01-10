ee6e4157979148def8a67ccb720467f7aaf5c5ac
vincent <526730329@qq.com>
2017-01-10 09:23:17 +0100

    Initial Commit

---
+++ HouseHolderMethod/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  HouseHolderMethod
+//
+//  Created by 袁萍 on 2017/1/10.
+//  Copyright © 2017年 袁萍. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
